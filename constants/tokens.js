71b8c8f15edd35b12005b374ef97791ee6c93e91
jkadar186 <jkadar186@gmail.com>
2021-11-29 05:31:56 +0500

    custom wrapped token address updated

--- constants/tokens.js
+++ constants/tokens.js
@@ -44,9 +44,9 @@ const PAYTOKENS = process.env.NETWORK_CHAINID === "250" ? [
   ] :
   [
     {
-      address: '0xf1277d1ed8ad466beddf92ef448a132661956621',
-      name: 'Wrapped Fantom',
-      symbol: 'wftm',
+      address: '0x2d3d8878229419F5A1486C772021D020e6FF7Ed7',
+      name: 'Wrapped Token',
+      symbol: 'wTKN',
       decimals: 18,
     },
   ]
