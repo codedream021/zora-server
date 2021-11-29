01bf1bd66750f2836965604d3e2c7dc31bdff98c
jkadar186 <jkadar186@gmail.com>
2021-11-29 05:31:32 +0500

    cloudfare ipfs url added

--- apis/transfertracker.js
+++ apis/transfertracker.js
@@ -343,7 +343,7 @@ router.post(
           let metadataURI = tokenURI;
           if (tokenURI.includes('ipfs://')) {
             let uri = tokenURI.split('//')[1];
-            metadataURI = `https://artion.mypinata.cloud/ipfs/${uri}`;
+            metadataURI = `https://cloudflare-ipfs.com/ipfs/${uri}`;
           }
           metadata = await axios.get(metadataURI);
           try {
